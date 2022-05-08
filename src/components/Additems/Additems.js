de76cdf696216a35a22abb5ba3bb1dfac3c82dee
mdriyadtalukder <riyadtalukder67@gmail.com>
2022-05-08 23:42:39 +0600

    finish

--- src/components/Additems/Additems.js
+++ src/components/Additems/Additems.js
@@ -58,41 +58,43 @@ const Additems = () => {
         }
     }
     return (
-        <div className='mt-3 mb-3'>
-            <h1 className='text-center fw-bold'>Add New Items</h1>
-            <Form id='additemform' onSubmit={addItem} className='w-50 mx-auto shadow-lg p-5 mt-3 mb-3'>
-                <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control readOnly type="email" value={user?.email} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicName">
-                    <Form.Label>Item Name</Form.Label>
-                    <Form.Control ref={namevalue} type="text" placeholder="Enter Name" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicImg">
-                    <Form.Label>Item Image</Form.Label>
-                    <Form.Control ref={imgvalue} type="text" placeholder="Enter Image Link" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicDescription">
-                    <Form.Label>Item Description</Form.Label>
-                    <Form.Control ref={descriptionvalue} type="text" placeholder="Enter Description" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPrice">
-                    <Form.Label> Item Price</Form.Label>
-                    <Form.Control ref={pricevalue} type="text" placeholder="Enter Price" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicQuantity">
-                    <Form.Label> Item Quantity</Form.Label>
-                    <Form.Control ref={quantityvalue} type="text" placeholder="Enter Quantity" />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicSupplierName">
-                    <Form.Label>Supplier Name</Form.Label>
-                    <Form.Control ref={suppliervalue} type="text" placeholder="Enter Supplier Name" />
-                </Form.Group>
+        <div>
+            <div className='mt-3 mb-3' style={{ minHeight: '90vh' }}>
+                <h1 className='text-center fw-bold'>Add New Items</h1>
+                <Form id='additemform' onSubmit={addItem} className='w-50 mx-auto shadow-lg p-5 mt-3 mb-3'>
+                    <Form.Group className="mb-3" controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control readOnly type="email" value={user?.email} />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicName">
+                        <Form.Label>Item Name</Form.Label>
+                        <Form.Control ref={namevalue} type="text" placeholder="Enter Name" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicImg">
+                        <Form.Label>Item Image</Form.Label>
+                        <Form.Control ref={imgvalue} type="text" placeholder="Enter Image Link" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicDescription">
+                        <Form.Label>Item Description</Form.Label>
+                        <Form.Control ref={descriptionvalue} type="text" placeholder="Enter Description" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicPrice">
+                        <Form.Label> Item Price</Form.Label>
+                        <Form.Control ref={pricevalue} type="text" placeholder="Enter Price" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicQuantity">
+                        <Form.Label> Item Quantity</Form.Label>
+                        <Form.Control ref={quantityvalue} type="text" placeholder="Enter Quantity" />
+                    </Form.Group>
+                    <Form.Group className="mb-3" controlId="formBasicSupplierName">
+                        <Form.Label>Supplier Name</Form.Label>
+                        <Form.Control ref={suppliervalue} type="text" placeholder="Enter Supplier Name" />
+                    </Form.Group>
 
-                <button className='btn button3 w-100'>Add Item</button>
-            </Form>
-            <ToastContainer ></ToastContainer>
+                    <button className='btn button3 w-100'>Add Item</button>
+                </Form>
+                <ToastContainer ></ToastContainer>
+            </div>
             <Footer></Footer>
         </div>
     );
